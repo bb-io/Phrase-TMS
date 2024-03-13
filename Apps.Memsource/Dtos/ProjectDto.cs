5b334c5294e85e2f92bbc98e313e4aca0a2796ee
Cecilia Nistal <cecilia.nistal@centific.com>
2024-03-12 22:17:32 -0300

    Add Client, Domain, Subdomain, Owner and Source Language to Get Project output

--- Apps.Memsource/Dtos/ProjectDto.cs
+++ Apps.Memsource/Dtos/ProjectDto.cs
@@ -1,9 +1,11 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
 
 namespace Apps.PhraseTMS.Dtos;
 
 public class ProjectDto
 {
+    public string Id { get; set; }
     public string? Name { get; set; }
 
     [Display("UID")]
@@ -11,6 +13,9 @@ public class ProjectDto
 
     [Display("Creation date")]
     public DateTime? DateCreated { get; set; }
+
+    [Display("Source language")]
+    public string sourceLang { get; set; }
 
     [Display("Target languages")]
     public List<string> TargetLangs { get; set; }
@@ -23,4 +28,61 @@ public class ProjectDto
 
     [Display("Note")]
     public string Note { get; set; }
+
+    [JsonProperty("domain")]
+    public Domain Domain { get; set; }
+
+    [JsonProperty("client")]
+    public Client Client { get; set; }
+
+    [JsonProperty("subDomain")]
+    public subDomain SubDomain { get; set; }
+
+    [JsonProperty("owner")]
+    public owner Owner { get; set; }
+
+}
+
+public class Domain 
+{
+    public string name { get; set; }
+
+    public string id { get; set; }
+
+    public string uid { get; set; }
+}
+
+public class subDomain
+{
+    public string name { get; set; }
+
+    public string id { get; set; }
+
+    public string uid { get; set; }
+}
+
+public class Client
+{
+    public string name { get; set; }
+
+    public string id { get; set; }
+
+    public string uid { get; set; }
+}
+
+public class owner
+{
+    public string firstName { get; set; }
+
+    public string lastName { get; set; }
+
+    public string userName { get; set; }
+
+    public string email { get; set; }
+
+    public string role { get; set; }
+
+    public string id { get; set; }
+
+    public string uid { get; set; }
 }
