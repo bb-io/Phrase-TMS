70debe09f8bd4ba6e819387b705218ceef1cd8b9
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-08-19 16:02:33 +0200

    Added optional input to assigned job event

---
+++ Apps.Memsource/Webhooks/Models/Requests/JobAssignedRequest.cs
@@ -0,0 +1,20 @@
+﻿using Apps.PhraseTMS.DataSourceHandlers;
+using Apps.PhraseTMS.DataSourceHandlers.StaticHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.PhraseTMS.Webhooks.Models.Requests
+{
+    public class JobAssignedRequest
+    {
+        [Display("User ID")]
+        [DataSource(typeof(UserDataHandler))]
+        public string? UserId { get; set; }
+    }
+}
