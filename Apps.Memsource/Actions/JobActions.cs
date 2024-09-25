9c9ebade31ad344c2c64f2109bab965731a4194e
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-09-25 14:16:24 +0300

    Fixed due date issue with create project actions

--- Apps.Memsource/Actions/JobActions.cs
+++ Apps.Memsource/Actions/JobActions.cs
@@ -148,9 +148,8 @@ public class JobActions
     {
         var client = new PhraseTmsClient(authenticationCredentialsProviders);
         var request = new PhraseTmsRequest($"/api2/v1/projects/{projectRequest.ProjectUId}/jobs/{input.JobUId}",
-            Method.Patch, authenticationCredentialsProviders);
-        request.WithJsonBody(body, JsonConfig.DateSettings);
-
+            Method.Patch, authenticationCredentialsProviders)
+            .WithJsonBody(body, JsonConfig.DateSettings);
         return client.ExecuteWithHandling(request);
     }
 
