8ab8ea47f48cf7bfdcfd3ec3db60dd33ee32d358
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-14 17:25:11 +0300

    Added on subscription event

--- Apps.Memsource/Webhooks/Handlers/ProjectHandlers/ProjectStatusChangedHandler.cs
+++ Apps.Memsource/Webhooks/Handlers/ProjectHandlers/ProjectStatusChangedHandler.cs
@@ -1,8 +1,54 @@
-﻿namespace Apps.PhraseTMS.Webhooks.Handlers.ProjectHandlers;
+﻿using Apps.PhraseTMS.Dtos;
+using Apps.PhraseTMS.Models.Projects.Requests;
+using Apps.PhraseTMS.Webhooks.Models.Requests;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using RestSharp;
 
-public class ProjectStatusChangedHandler : BaseWebhookHandler
+namespace Apps.PhraseTMS.Webhooks.Handlers.ProjectHandlers;
+
+public class ProjectStatusChangedHandler : BaseWebhookHandler, IAfterSubscriptionWebhookEventHandler<ProjectDto>
 {
     const string SubscriptionEvent = "PROJECT_STATUS_CHANGED";
+    private readonly ProjectOptionalRequest _projectOptionalRequest;
+    private readonly ProjectStatusChangedRequest _projectStatusChangedRequest;
+    private readonly InvocationContext _invocationContext;
+    
+    public ProjectStatusChangedHandler([WebhookParameter] ProjectStatusChangedRequest request, 
+        [WebhookParameter] ProjectOptionalRequest project,
+        InvocationContext invocationContext) : base(SubscriptionEvent)
+    {
+        _projectOptionalRequest = project;
+        _projectStatusChangedRequest = request;
+        _invocationContext = invocationContext;
+    }
+    
+    public async Task<AfterSubscriptionEventResponse<ProjectDto>> OnWebhookSubscribedAsync()
+    {
+        if (_projectOptionalRequest.ProjectUId != null && _projectStatusChangedRequest.Status != null)
+        {
+            var client = new PhraseTmsClient(_invocationContext.AuthenticationCredentialsProviders);
+            var request = new PhraseTmsRequest($"/api2/v1/projects/{_projectOptionalRequest.ProjectUId}", Method.Get,
+                _invocationContext.AuthenticationCredentialsProviders);
+            var project = await client.ExecuteWithHandling<ProjectDto>(request);
+            
+            if(project.Status == _projectStatusChangedRequest.Status)
+            {
+                return new AfterSubscriptionEventResponse<ProjectDto>()
+                {
+                    Result = project
+                };
+            }
+            
+            return new AfterSubscriptionEventResponse<ProjectDto>()
+            {
+                Result = null
+            };
+        }
 
-    public ProjectStatusChangedHandler() : base(SubscriptionEvent) { }
+        return new AfterSubscriptionEventResponse<ProjectDto>()
+        {
+            Result = null
+        };
+    }
 }
