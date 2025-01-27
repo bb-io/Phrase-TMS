4a35a4f403f732c50390eb7fc00df2ec4ad9a240
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-01-27 19:57:57 +0100

    Json to error message exception body

--- Apps.Memsource/PhraseTmsClient.cs
+++ Apps.Memsource/PhraseTmsClient.cs
@@ -95,7 +95,7 @@ public class PhraseTmsClient : RestClient
         if (!String.IsNullOrEmpty(response.ErrorMessage) && response.ErrorMessage.Contains("wasnt found"))
             throw new PluginMisconfigurationException(response.ErrorMessage + "Please check the inputs for this action");
 
-        throw new PluginApplicationException(response.Content);
+        throw new PluginApplicationException(response?.ErrorMessage);
     }
         
     public async Task<List<T>> Paginate<T>(RestRequest request)
