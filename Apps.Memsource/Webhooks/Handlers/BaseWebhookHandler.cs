ddb73249d25f57463e363038617ab967a0b754d2
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2025-02-05 10:56:10 +0200

    Code cleanup

--- Apps.Memsource/Webhooks/Handlers/BaseWebhookHandler.cs
+++ Apps.Memsource/Webhooks/Handlers/BaseWebhookHandler.cs
@@ -14,12 +14,7 @@ public class BaseWebhookHandler(InvocationContext invocationContext, string subE
 {
     public async Task SubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProvider,
         Dictionary<string, string> values)
-    {        
-        await WebhookLogger.LogAsync(new
-        {
-            status = "Called SubscribeAsync"
-        });
-        
+    {    
         var client = new PhraseTmsClient(authenticationCredentialsProvider);
         var request = new PhraseTmsRequest($"/api2/v2/webhooks", Method.Post, authenticationCredentialsProvider);
         request.WithJsonBody(new
@@ -30,78 +25,25 @@ public class BaseWebhookHandler(InvocationContext invocationContext, string subE
         });
 
         await client.ExecuteWithHandling(request);
-
-        await WebhookLogger.LogAsync(new
-        {
-            status = "successfully subscribed",
-            events = new[] { subEvent },
-            url = values["payloadUrl"],
-            name = subEvent
-        });
     }
 
     public async Task UnsubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProvider,
         Dictionary<string, string> values)
     {
-        try
-        {
-            var currentRetry = 0;
-            await UnsubscribeRecursivelyAsync(authenticationCredentialsProvider, values, currentRetry);
-            await Task.Delay(15000);
-            
-            await WebhookLogger.LogAsync(new
-            {
-                status = "delay is gone"
-            });
-        }
-        catch (Exception e)
-        {
-            await WebhookLogger.LogAsync(new
-            {
-                status = "failed",
-                message = e.Message,
-                stack_trace = e.StackTrace
-            });
-
-            throw;
-        }
-    }
-
-    private async Task UnsubscribeRecursivelyAsync(
-        IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProvider,
-        Dictionary<string, string> values,
-        int currentRetry)
-    {
-        await WebhookLogger.LogAsync(new
-        {
-            status = $"Called UnsubscribeAsync {currentRetry}",
-            values
-        });
-
+        
         var authenticationCredentialsProviders = authenticationCredentialsProvider as AuthenticationCredentialsProvider[] ?? authenticationCredentialsProvider.ToArray();
+        
         var client = new PhraseTmsClient(authenticationCredentialsProviders);
         var getRequest = new PhraseTmsRequest($"/api2/v2/webhooks?name={subEvent}&url={values["payloadUrl"]}",
             Method.Get, authenticationCredentialsProviders);
         var webhooks = await client.ExecuteWithHandling<ResponseWrapper<List<WebhookDto>>>(getRequest);
         var webhookUId = webhooks?.Content.FirstOrDefault()?.UId;
 
-        await WebhookLogger.LogAsync(new
-        {
-            status = $"After getting webhooks {currentRetry}",
-            webhooks,
-            webhookUId
-        });
-
         if (webhookUId == null)
             return;
 
         var deleteRequest = new PhraseTmsRequest($"/api2/v2/webhooks/{webhookUId}", Method.Delete,
             authenticationCredentialsProviders);
-        var deleteResponse = await client.ExecuteWithHandling(deleteRequest);
-        await WebhookLogger.LogAsync(new
-        {
-            status = $"successfully deleted webhook {currentRetry}",
-            deleteResponse.Content
-        });
+        await client.ExecuteWithHandling(deleteRequest);
     }
 }
