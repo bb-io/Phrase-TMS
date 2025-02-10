858f1fbbb9e7974268cfd754b11f0b249c5fdc3e
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-10 11:13:14 +0200

    Added unit tests

---
+++ Tests.PhraseTMS/DataSources.cs
@@ -0,0 +1,14 @@
+﻿using PhraseTMSTests.Base;
+
+namespace Tests.PhraseTMS
+{
+    [TestClass]
+    public class DataSources : TestBase
+    {
+        [TestMethod]
+        public async Task DictionaryDataHandlerReturnsValues()
+        {
+
+        }
+    }
+}
