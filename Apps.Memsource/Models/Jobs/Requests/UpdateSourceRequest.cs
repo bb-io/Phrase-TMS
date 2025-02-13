1cd9320e3548a607e9b3239fed62b45cebb35823
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-02-13 14:23:19 +0100

    Add UpdateSource feature WIP

---
+++ Apps.Memsource/Models/Jobs/Requests/UpdateSourceRequest.cs
@@ -0,0 +1,20 @@
+﻿using Apps.PhraseTMS.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.PhraseTMS.Models.Jobs.Requests
+{
+    public class UpdateSourceRequest
+    {
+        [Display("Job ID")]
+        [DataSource(typeof(JobDataHandler))]
+        public string JobUId { get; set; }
+
+        [Display("File")]
+        public FileReference File { get; set; }
+
+        [Display("Should the file be pre-translated?")]
+        public bool? preTranslate { get; set; }
+    }
+}
---
+++ Apps.Memsource/Models/Jobs/Responses/UpdateSourceResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.PhraseTMS.Models.Jobs.Responses
+{
+    public class UpdateSourceResponse
+    {
+        [Display("Job ID")] public string Uid { get; set; }
+
+        [Display("File name")] public string Filename { get; set; }
+
+        public string Status { get; set; }
+
+        [Display("Target language")] public string TargetLang { get; set; }
+    }
+}
