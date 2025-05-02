9926cd07e6ffb41cedad9cdfd5606d43a9d06b19
ce-nistal <cecilia.nistal@blackbird.io>
2025-05-02 13:23:53 -0300

    Edited job status handler for Update job action

---
+++ Apps.Memsource/DataSourceHandlers/StaticHandlers/UpdateJobStatusDataHandler.cs
@@ -0,0 +1,24 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.PhraseTMS.DataSourceHandlers.StaticHandlers;
+
+public class UpdateJobStatusDataHandler : IStaticDataSourceItemHandler
+{
+    private static Dictionary<string, string> Data => new()
+    {
+        { "ACCEPTED", "Accepted" },
+        { "CANCELLED", "Cancelled" },
+        { "COMPLETED", "Completed" },
+        { "DECLINED", "Declined" },
+        { "DELIVERED", "Delivered" },
+        { "EMAILED", "Emailed" },
+        { "NEW", "New" },
+        { "REJECTED", "Rejected" },
+    };
+
+    public IEnumerable<DataSourceItem> GetData()
+    {
+        return Data.Select(x => new DataSourceItem(x.Key, x.Value));
+    }
+}
