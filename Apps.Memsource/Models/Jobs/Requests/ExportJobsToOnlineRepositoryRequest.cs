3bdf5c4823ace03d0689cc5e24debcbb8c70b991
RiabushenkoA <ariabushenko@blackbird.io>
2025-05-22 12:39:29 +0300

    Added action 'Export jobs to online repository'

--- Apps.Memsource/Models/Jobs/Requests/ExportJobsToOnlineRepositoryRequest.cs
+++ Apps.Memsource/Models/Jobs/Requests/ExportJobsToOnlineRepositoryRequest.cs
@@ -1,7 +1,13 @@
-﻿namespace Apps.PhraseTMS.Models.Jobs.Requests
+﻿using Apps.PhraseTMS.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.PhraseTMS.Models.Jobs.Requests
 {
     public class ExportJobsToOnlineRepositoryRequest
     {
+        [Display("Job IDs")]
+        [DataSource(typeof(JobDataHandler))]
         public IEnumerable<string> JobIds { get; set; }
     }
 }
