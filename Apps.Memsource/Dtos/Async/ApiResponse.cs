58d28d7120ccffac3dafac13287e68639cda043b
ce-nist <cecilia.nistal@blackbird.io>
2025-10-28 17:57:58 -0300

    Catch errors during job update

---
+++ Apps.Memsource/Dtos/Async/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace Apps.PhraseTMS.Dtos.Async;
+
+public class ApiResponse
+{
+
+    [JsonPropertyName("updated")]
+    public int? Updated { get; set; }
+
+    [JsonPropertyName("errors")]
+    public List<string>? Errors { get; set; } = new();
+    
+}
