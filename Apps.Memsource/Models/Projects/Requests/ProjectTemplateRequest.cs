cbc45d31e985fa47de2ecdf378c25bd58b897b05
RiabushenkoA <ariabushenko@blackbird.io>
2025-11-20 12:37:59 +0200

    Added Project template actions

--- Apps.Memsource/Models/Projects/Requests/ProjectTemplateRequest.cs
+++ Apps.Memsource/Models/Projects/Requests/ProjectTemplateRequest.cs
@@ -1,10 +1,13 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.PhraseTMS.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.PhraseTMS.Models.Projects.Requests
 {
     public class ProjectTemplateRequest
     {
         [Display("Project template UID")]
+        [DataSource(typeof(ProjectTemplateDataHandler))]
         public string ProjectTemplateUId { get; set; } = default!;
     }
 }
