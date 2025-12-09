109d558af08905398226a1e55292cc33aecfbe81
Artem Riabushenko <ariabushenko@blackbird.io>
2025-12-09 13:56:07 +0200

    Added action 'Delete analyses'

---
+++ Apps.Memsource/Models/Analysis/Requests/DeleteAnalysesRequest.cs
@@ -0,0 +1,10 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.PhraseTMS.Models.Analysis.Requests
+{
+    public class DeleteAnalysesRequest
+    {
+        [Display("Analyses IDs", Description = "Optional. If empty, all analyses will be removed.")]
+        public IEnumerable<string>? AnalysesIds { get; set; }
+    }
+}
---
+++ Apps.Memsource/Models/Analysis/Responses/DeleteAnalysesResponse.cs
@@ -0,0 +1,21 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.PhraseTMS.Models.Analysis.Responses
+{
+    public class DeleteAnalysesResponse
+    {
+        [Display("Deleted analyses IDs")]
+        public IEnumerable<string> DeletedAnalysesIds { get; set; } = new List<string>();
+
+        [Display("Errors during removal")]
+        public IEnumerable<DeleteAnalysisError> Errors { get; set; } = new List<DeleteAnalysisError>();
+    }
+
+    public class DeleteAnalysisError
+    {
+        [Display("Analysis ID")]
+        public string AnalysisId { get; set; } = default!;
+
+        public string Error { get; set; } = default!;
+    }
+}
