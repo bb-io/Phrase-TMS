d49dd8dc9335df45ca5391eead6d0e995e3627fd
Alex Terehov <terales@users.noreply.github.com>
2026-06-09 11:41:48 -0230

    Introduce 'Download interoperable XLIFF' experimental action

---
+++ Apps.Memsource/Actions/InteroperableXliffActions.cs
@@ -0,0 +1,100 @@
+﻿using Apps.PhraseTMS.Extensions;
+using Apps.PhraseTMS.Helpers;
+using Apps.PhraseTMS.Models.InteroperableXliff;
+using Apps.PhraseTMS.Models.Jobs.Requests;
+using Apps.PhraseTMS.Models.Projects.Requests;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Exceptions;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Utils.Extensions.Files;
+using Blackbird.Applications.Sdk.Utils.Extensions.Http;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using Blackbird.Filters.Enums;
+using Blackbird.Filters.Extensions;
+using Blackbird.Filters.Transformations;
+using Blackbird.Filters.Xliff.Xliff1;
+using Blackbird.Filters.Xliff.Xliff2;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Text;
+
+namespace Apps.PhraseTMS.Actions;
+
+[ActionList("Interoperable XLIFF (experimental)")]
+public class InteroperableXliffActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient) : PhraseInvocable(invocationContext)
+{
+    [Action("Download interoperable XLIFF (experimental)", Description = "Export XLIFF file with normalized metadata and content, so it could be reused in all other actions.")]
+    public async Task<DownloadInteroperableXliffResponse> DownloadInteroperableXliff(
+        ProjectRequest projectInput,
+        JobRequest jobInput)
+    {
+        var requestFile = new RestRequest($"/api2/v1/projects/{projectInput.ProjectUId}/jobs/bilingualFile", Method.Post)
+            .WithJsonBody(new
+            {
+                jobs = new[]
+                {
+                    new { uid = jobInput.JobUId }
+                },
+            });
+
+        var responseFile = await Client.ExecuteWithHandling(requestFile);
+
+        var fileData = responseFile.RawBytes;
+        var fileName = responseFile.GetFilenameFromHeader("default_target_file");
+
+        if ((fileData?.LongLength ?? 0) == 0)
+            throw new PluginApplicationException("Empty file received from Phrase TMS.");
+
+        var fileString = Encoding.UTF8.GetString(fileData ?? []);
+        var transformation = Transformation.Parse(fileString, fileName);
+
+        var response = new DownloadInteroperableXliffResponse()
+        {
+            SourceLocale = transformation.SourceLanguage ?? string.Empty,
+            TargetLocale = transformation.TargetLanguage ?? string.Empty
+        };
+
+        foreach (var unit in transformation.GetUnits())
+        {
+            response.TotalSegments += unit.Segments.Count;
+
+            // Normalize segment states
+            var (targetState, updateCount) = unit switch
+            {
+                _ when MXLIFFHelper.IsLocked(unit) => (SegmentState.Final, (Action<DownloadInteroperableXliffResponse>)(r => r.LockedSegments++)),
+                _ when MXLIFFHelper.IsConfirmed(unit) => (SegmentState.Reviewed, (Action<DownloadInteroperableXliffResponse>)(r => r.ConfirmedSegments++)),
+                _ when unit.Segments.Any(s => !string.IsNullOrEmpty(s.GetTarget())) => (SegmentState.Translated, null),
+                _ => (SegmentState.Initial, (Action<DownloadInteroperableXliffResponse>)(r => r.EmptyTargetSegments++))
+            };
+
+            foreach (var segment in unit.Segments)
+            {
+                segment.State = targetState;
+                updateCount?.Invoke(response);
+            }
+
+            // Normalize character limits
+            if (MXLIFFHelper.GetMaxLen(unit, transformation) is int characterLimit)
+                unit.SizeRestrictions.MaximumSize = characterLimit;
+
+            // Normalize scores
+            if (double.TryParse(MXLIFFHelper.GetQeScore(unit), out var qeScore))
+            {
+                unit.Quality.Score = qeScore;
+                unit.Quality.ProfileReference = "QE by Phrase TMS";
+            }
+
+            // Normalize metadata
+            if (MXLIFFHelper.GetOrigin(unit) is string origin)
+                unit.Provenance.Translation.Tool = $"Phrase TMS ({origin})";
+        }
+
+        response.File = await fileManagementClient.UploadAsync(
+            transformation.Serialize().ToStream(),
+            "application/xliff+xml",
+            fileName.Replace(".mxliff", ".xliff"));
+
+        return response;
+    }
+}
---
+++ Tests.PhraseTMS/InteroperableXliffActionsTests.cs
@@ -0,0 +1,33 @@
+﻿
+using Apps.PhraseTMS.Actions;
+using Apps.PhraseTMS.Models.Jobs.Requests;
+using Apps.PhraseTMS.Models.Projects.Requests;
+using Blackbird.Applications.Sdk.Common.Files;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Filters.Transformations;
+using PhraseTMSTests.Base;
+
+namespace Tests.PhraseTMS;
+
+[TestClass]
+public class InteroperableXliffActionsTests : TestBaseMultipleConnections
+{
+    public const string PROJECT_ID = "0dmkbDfIkgTMrOgCf3l5c0";
+    public const string JOB_ID = "a4n2zcUF4gaFb5MpTTXTl2";
+
+    [TestMethod, ContextDataSource]
+    public async Task DownloadInteroperableXliff_IsSuccess(InvocationContext context)
+    {
+        // Arrange
+        var actions = new InteroperableXliffActions(context, FileManager);
+        var project = new ProjectRequest { ProjectUId = PROJECT_ID };
+        var job = new JobRequest { JobUId = JOB_ID };
+
+        // Act
+        var result = await actions.DownloadInteroperableXliff(project, job);
+
+        // Assert
+        Console.WriteLine(result.File.Name);
+        Assert.AreEqual(6, result.TotalSegments);
+    }
+}
